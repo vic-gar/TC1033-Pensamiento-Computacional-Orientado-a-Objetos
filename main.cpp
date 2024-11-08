9475d10bab1a84a37814f0ceaa632e13d96f9ec7
vic-gar <vagarciag05@gmail.com>
2024-11-07 23:34:25 -0600

    Upload Proyecto (avance) Creación de Objetos

--- main.cpp
+++ main.cpp
@@ -1,8 +1,27 @@
-#include<iostream>
-#include<string>
-#include"animales.h"
+/*
+  * Proyecto Casa de Apoción de mascotas
+  * Víctor Adrián García Galván
+  * A01713062
+  * 07/11/2024
+*/
 
+/**
+  * Descripción del Proyecto
+  * Este proyecto para la clase de Pensamiento Computacional Orientado a Objetos
+  * es un programa que guarda diferentes tipos de animales en una casa de
+  * acojida con sus respectivos nombres, razas, edades, peso y datos de su
+  * salud, y nos permite calcular la cantidad de comida necesaria dependiendo
+  * del peso del animal.
+**/
+
+//Bibliotecas
+#include<iostream> //Para imprimir
+#include<string>
+#include"animales.h" // Objetos de mi proyecto
+
+//Main del programa 
 int main(){
+
   Perro perro_1("Chihuahua", "Pedro", 10, 3.75, true);
   std::cout << perro_1.get_raza() << std::endl;
   std::cout << perro_1.get_nombre() << std::endl;
