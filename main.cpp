71eebf428a34c76e045f46af9043a5777edab51c
vic-gar <vagarciag05@gmail.com>
2024-11-14 21:12:04 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,41 +10,47 @@
   * Este proyecto para la clase de Pensamiento Computacional Orientado a Objetos
   * es un programa que guarda diferentes tipos de animales en una casa de
   * acojida con sus respectivos nombres, razas, edades, peso y datos de su
-  * salud, y nos permite calcular la cantidad de comida necesaria dependiendo
-  * del peso del animal.
+  * alimentación, calculando su dieta dependiendo de que animal es y de su peso.
 **/
 
 //Bibliotecas
-#include<iostream> //Para imprimir
+#include<iostream>
 #include<string>
-#include"animales.h" // Objetos de mi proyecto
 
-//Main del programa 
+#include"Perro.h" // Objetos de mi proyecto
+#include"Gato.h"
+#include"Conejo.h"
+
+
+//Main del programa
 int main(){
 
-  Perro perro_1("Chihuahua", "Pedro", 10, 3.75, true);
-  std::cout << perro_1.get_raza() << std::endl;
-  std::cout << perro_1.get_nombre() << std::endl;
-  std::cout << perro_1.get_edad() << std::endl;
-  std::cout << perro_1.get_peso() << std::endl;
-  std::cout << perro_1.get_vacunacion() << std::endl;
-  std::cout << perro_1.get_cantidad_alimento() << "\n" << std::endl;
+  Perro perro_1("Pedro", 10, 3.75, "Chihuahua");
+  std::cout << "Esta es la informacion del Perro 1" << std::endl;
+  std::cout << "Nombre: " << perro_1.get_nombre() << std::endl;
+  std::cout << "Edad: " << perro_1.get_edad() << std::endl;
+  std::cout << "Peso: " << perro_1.get_peso() << "kg" << std::endl;
+  std::cout << "Raza: " << perro_1.get_raza() << std::endl;
+  std::cout << "Esta es la cantidad de alimento que debe consumir" << std::endl;
+  std::cout << perro_1.cantidadAlimentoPerro() << "g\n" << std::endl;
 
-  Gato gato_1("Siames", "Jorge", 6, 6.5, true);
-  std::cout << gato_1.get_raza() << std::endl;
-  std::cout << gato_1.get_nombre() << std::endl;
-  std::cout << gato_1.get_edad() << std::endl;
-  std::cout << gato_1.get_peso() << std::endl;
-  std::cout << gato_1.get_vacunacion() << std::endl;
-  std::cout << gato_1.get_cantidad_alimento() << "\n" << std::endl;
+  Gato gato_1("Jorge", 6, 6.5, "Siames");
+  std::cout << "Esta es la informacion del Gato 1" << std::endl;
+  std::cout << "Nombre: " << gato_1.get_nombre() << std::endl;
+  std::cout << "Edad: " << gato_1.get_edad() << std::endl;
+  std::cout << "Peso: " << gato_1.get_peso() << "kg" << std::endl;
+  std::cout << "Raza: " << gato_1.get_raza() << std::endl;
+  std::cout << "Esta es la cantidad de alimento que debe consumir" << std::endl;
+  std::cout << gato_1.cantidadAlimentoGato() << "g\n" << std::endl;
 
-  Conejo conejo_1("Mini Rex", "Destructor", 4, 1.4, false);
-  std::cout << conejo_1.get_raza() << std::endl;
-  std::cout << conejo_1.get_nombre() << std::endl;
-  std::cout << conejo_1.get_edad() << std::endl;
-  std::cout << conejo_1.get_peso() << std::endl;
-  std::cout << conejo_1.get_vacunacion() << std::endl;
-  std::cout << conejo_1.get_cantidad_alimento() << "\n" << std::endl;
+  Conejo conejo_1("Destructor", 4, 1.1, "Mini Rex");
+  std::cout << "Esta es la informacion del Conejo 1" << std::endl;
+  std::cout << "Nombre: " << conejo_1.get_nombre() << std::endl;
+  std::cout << "Edad: " << conejo_1.get_edad() << std::endl;
+  std::cout << "Peso: " << conejo_1.get_peso() << "kg" << std::endl;
+  std::cout << "Raza: " << conejo_1.get_raza() << std::endl;
+  std::cout << "Esta es la cantidad de alimento que debe consumir" << std::endl;
+  std::cout << conejo_1.cantidadAlimentoConejo() << "g\n" << std::endl;
 
   return 0;
 }
